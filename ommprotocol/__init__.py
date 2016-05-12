51a9f755f42c36fe92847ba2666fc70b35c5e1d8
Jaime RGP <jaime.rogue@gmail.com>
2016-05-12 16:26:57 +0200

    New main function in __init__

--- ommprotocol/__init__.py
+++ ommprotocol/__init__.py
@@ -1,1 +1,19 @@
-from .ommprotocol import run_protocol
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#################################################
+#           insiliChem OpenMM launcher          #
+# --------------------------------------------- #
+# By Jaime RGP <jaime@insilichem.com> @ 2016    #
+#################################################
+
+from ommprotocol.io import prepare_input
+from ommprotocol.core import protocol
+
+
+def run_protocol():
+    handler, cfg = prepare_input()
+    protocol(handler, cfg)
+
+if __name__ == '__main__':
+    run_protocol()
