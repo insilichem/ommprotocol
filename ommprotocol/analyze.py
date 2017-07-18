f51c3b49d3ce73fb6d1856eeb05480e9ae425d28
Jaime RGP <jaime.rogue@gmail.com>
2017-07-18 15:42:59 +0200

    fix(analyze): allow arbitrary number of log or trajectory files

--- ommprotocol/analyze.py
+++ ommprotocol/analyze.py
@@ -19,20 +19,21 @@ plt.ioff()
 from .utils import extant_file
 
 
-def plot_log(path):
+def plot_log(paths):
     import pandas as pd
-    df = pd.read_csv(path, sep="\t", index_col=1)
+    multi_csv = (pd.read_csv(path, sep="\t", index_col=1) for path in paths)
+    df = pd.concat(multi_csv, ignore_index=True)
     df.ix[:, 1:5].plot(subplots=True, layout=(2,2))
     plt.show()
 
 
-def plot_rmsd(trajectory, topology):
+def plot_rmsd(topology, trajectory):
     import mdtraj
-    t = mdtraj.load(trajectory, top=topology)
+    t = mdtraj.load(trajectory, discard_overlapping_frames=True, top=topology)
     rmsd = mdtraj.rmsd(t, t)*10.0 # nm->Angstroms!
     plt.plot(rmsd)
     fig = plt.gca()
-    fig.set_title(trajectory)
+    fig.set_title(', '.join(trajectory))
     fig.set_xlabel('Frames')
     fig.set_ylabel('RMSD (A)')
     plt.show()
@@ -44,22 +45,24 @@ def main():
     
     # 'log' subcommand
     p_log = sp.add_parser('log', help='Plot .log reports')
-    p_log.add_argument('path', type=extant_file)
+    p_log.add_argument('paths', type=extant_file, nargs='+')
     p_log.set_defaults(func=plot_log)
 
     # 'rmsd' subcommand
     p_rmsd = sp.add_parser('rmsd', help='Plot RMSD of a trajectory')
-    p_rmsd.add_argument('trajectory', help='Trajectory file', type=extant_file)
     p_rmsd.add_argument('topology', help='Topology file', type=extant_file)
+    p_rmsd.add_argument('trajectory', help='Trajectory file', type=extant_file, 
+                        nargs='+')
     p_rmsd.set_defaults(func=plot_rmsd)
 
     # Autocall each requested method
-    args = p.parse_args()
-    if not hasattr(args, 'func'):
+    cli_args = p.parse_args()
+    if not hasattr(cli_args, 'func'):
         p.print_help()
         sys.exit()
-    fcode = args.func.__code__
-    args.func(*[getattr(args, arg) for arg in fcode.co_varnames[:fcode.co_argcount]])
+    f_code = cli_args.func.__code__
+    f_args = [getattr(cli_args, a) for a in f_code.co_varnames[:f_code.co_argcount]]
+    cli_args.func(*f_args)
 
 
 if __name__ == "__main__":
