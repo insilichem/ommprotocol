162ca1d52bcebe10f1b4a3012b480f82443145fe
Jaime RGP <jaime.rogue@gmail.com>
2017-12-18 17:23:57 +0100

    tests: updated gromacs test to be run only if gromacs is installed

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,6 +6,7 @@
 # By Jaime RGP <@jaimergp>
 
 import pytest
+from distutils.spawn import find_executable
 from ommprotocol.io import SystemHandler, Positions
 from ommprotocol.md import Stage
 from conftest import get_file
@@ -21,11 +22,10 @@ STAGE_OPTIONS = dict(steps=100,
                      save_state_at_end=False,
                      attempt_rescue=False)
 
-@pytest.mark.parametrize("top, pos", [
-    ('input.pdb', None),
-    ('input.prmtop', 'input.inpcrd'),
-    ('input.top', 'input.gro')
-])
+inputs = [('input.pdb', None), ('input.prmtop', 'input.inpcrd')]
+if find_executable('mdrun'):
+    inputs.append(('input.top', 'input.gro'))
+@pytest.mark.parametrize("top, pos", inputs)
 def test_formats(top, pos):
     top = get_file(top)
     kw = dict(positions=Positions.load(get_file(pos))) if pos else {}
