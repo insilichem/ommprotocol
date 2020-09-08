8a5e9e44340861e29d05191bf7d6d58f8e814144
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-09-08 17:51:40 +0200

    Update conda recipe

--- devtools/conda-recipe/bld.bat
+++
@@ -1,6 +0,0 @@
-if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\menu-windows.json" "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\app.ico" "%PREFIX%\Menu"
-
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
